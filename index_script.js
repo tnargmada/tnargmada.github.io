bedadeefe0745251cf7906d88c1ea8f76257cd25
tnargmada <adam123grant@gmail.com>
2023-10-17 16:45:44 -0500

    create custom classes for shapes

--- index_script.js
+++ index_script.js
@@ -3,27 +3,23 @@ const WALL_SIZE = 10000;
 // use this variable to distinguish a "drag" from a "click"
 var startDragTime;
 
-// module aliases
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
-    MouseConstraint = Matter.MouseConstraint,
-    Mouse = Matter.Mouse,
-    Composite = Matter.Composite;
-    Events = Matter.Events;
-
 // create an engine
-var engine = Engine.create();
+var engine = Matter.Engine.create();
 
 // get wrapper element
 var wrapper = document.getElementById("wrap");
+var circleElem = document.getElementById("circle");
+var triangleElem = document.getElementById("triangle");
 // also get current wrapper width & height
 var currentWidth = wrapper.clientWidth;
 var currentHeight = wrapper.clientHeight;
 
+// create shapes
+var square = new Square(wrapper, 150 + wrapper.clientWidth / 5, "#e83544", engine);
+var circle = new Circle(wrapper, 150 + wrapper.clientWidth / 5, "#356ee8", engine);
+
 // create a renderer
-var render = Render.create({
+var render = Matter.Render.create({
     element: wrapper,
     engine: engine,
     options: {
@@ -35,80 +31,54 @@ var render = Render.create({
 });
 
 // initialize variables to reference objects we'll need
-var square, circle, triangle, ground, leftWall, rightWall;
-
-function initializeShapes() {
-    // create two boxes
-    size = 150 + wrapper.clientWidth / 5;
-    square = Bodies.rectangle(0.5 * wrapper.clientWidth, size * -1.8, size, size, {render: { fillStyle: '#e83544' }});
-    circle = Bodies.circle(0.2 * wrapper.clientWidth, size * -2.3, size / 2, {render: { fillStyle: '#356ee8' }});
-    triangle = Bodies.polygon(0.8 * wrapper.clientWidth, size * -3, 3, size * 2 / 3, {render: { fillStyle: '#ffdf29' }});
-    Matter.Body.rotate(triangle, Math.PI / 2);
-    // launch them a little bit to the side 
-    Matter.Body.rotate(square, (Math.random() - 0.5) * Math.PI / 4);
-    Matter.Body.rotate(circle, (Math.random() - 0.5) * Math.PI / 4);
-    Matter.Body.rotate(triangle, (Math.random() - 0.5) * Math.PI / 4);
-    Matter.Body.setVelocity(square, Matter.Vector.create((Math.random() - 0.4) * 15, 5));
-    Matter.Body.setVelocity(circle, Matter.Vector.create((Math.random() - 0.4) * 15, 5));
-    Matter.Body.setVelocity(triangle, Matter.Vector.create((Math.random() - 0.4) * 15, 5));
-    // add to world
-    Composite.add(engine.world, [square, circle, triangle]);
-}
-
-function resetShapeSizes() {
-    
-}
+var ground, leftWall, rightWall;
 
 function initializeWalls() {
     // create a ground and walls
-    ground = Bodies.rectangle(
+    ground = Matter.Bodies.rectangle(
         wrapper.clientWidth / 2, wrapper.clientHeight + WALL_SIZE / 2,
         wrapper.clientWidth + 1000, WALL_SIZE,
         { isStatic: true, render: { visible: false } }
     );
-    rightWall = Bodies.rectangle(
+    rightWall = Matter.Bodies.rectangle(
         wrapper.clientWidth + WALL_SIZE / 2, wrapper.clientHeight / 2,
         WALL_SIZE, wrapper.clientHeight + 100000,
         { isStatic: true, render: { visible: false } }
     );
-    leftWall = Bodies.rectangle(
+    leftWall = Matter.Bodies.rectangle(
         -1 * WALL_SIZE / 2, wrapper.clientHeight / 2,
         WALL_SIZE, wrapper.clientHeight + 100000,
         { isStatic: true, render: { visible: false } }
     );
     // add to world
-    Composite.add(engine.world, [ground, rightWall, leftWall]);
+    Matter.Composite.add(engine.world, [ground, rightWall, leftWall]);
 }
 
-initializeShapes();
 initializeWalls();
-resetShapeSizes();
 
 // add mouse control
-var mouse = Mouse.create(render.canvas),
-mouseConstraint = MouseConstraint.create(engine, {
-    mouse: mouse,
-    constraint: {
-        stiffness: 0.2,
-        render: {
-            visible: false
-        }
-    }
-});
+var mouse = Matter.Mouse.create(render.canvas),
+mouseConstraint = Matter.MouseConstraint.create(engine, { element: wrapper });
 
-Composite.add(engine.world, mouseConstraint);
+Matter.Composite.add(engine.world, mouseConstraint);
 
 // keep the mouse in sync with rendering
 render.mouse = mouse;
 
 // run the renderer
-Render.run(render);
+// Render.run(render);
+(function render() {
+    window.requestAnimationFrame(render);
+    // update shapes based on engine values
+    square.updateElem();
+    circle.updateElem();
+})();
 
 // create runner
-var runner = Runner.create();
+var runner = Matter.Runner.create();
 
 // run the engine
-Runner.run(runner, engine);
+Matter.Runner.run(runner, engine);
 
 function handleResize() {
     widthRatio = wrapper.clientWidth / currentWidth;
@@ -121,9 +91,6 @@ function handleResize() {
     render.options.height = wrapper.clientHeight;
     render.canvas.width = wrapper.clientWidth;
     render.canvas.height = wrapper.clientHeight;
-    Matter.Body.scale(square, widthRatio, widthRatio);
-    Matter.Body.scale(circle, widthRatio, widthRatio);
-    Matter.Body.scale(triangle, widthRatio, widthRatio);
     Matter.Body.setPosition(
         ground,
         Matter.Vector.create(
@@ -152,11 +119,11 @@ function handleResize() {
 
 window.addEventListener("resize", handleResize);
 
-Events.on(mouseConstraint, "startdrag", () => {
+Matter.Events.on(mouseConstraint, "startdrag", () => {
     startDragTime = Date.now();
 })
 
-Events.on(mouseConstraint, "enddrag", (event) => {
+Matter.Events.on(mouseConstraint, "enddrag", (event) => {
     endDragTime = Date.now();
     if (endDragTime - startDragTime < 200) {
         if (event.body.id == 1) {
---
+++ shapes.js
@@ -0,0 +1,50 @@
+class Square {
+    constructor(parentElem, width, color, matterEngine) {
+        this.matterEngine = matterEngine;
+        // initialize DOM element
+        this.element = document.createElement("div");
+        parentElem.appendChild(this.element);
+        // style DOM element
+        this.element.style.width = `${width}px`;
+        this.element.style.height = `${width}px`;
+        this.element.style.backgroundColor = color;
+        this.element.style.position = "absolute";
+        // initialize matter.js body
+        this.body = Matter.Bodies.rectangle(0.5 * parentElem.clientWidth, width * -1.8, width, width);
+        // add matter.js body to matter.js world
+        Matter.Composite.add(this.matterEngine.world, this.body);
+    }
+    // method to update DOM element based on matter.js engine, for rendering
+    updateElem() {
+        // set position and rotation
+        this.element.style.top = `${this.body.position.y - this.element.clientHeight / 2}px`;
+        this.element.style.left = `${this.body.position.x - this.element.clientWidth / 2}px`;
+        this.element.style.transform = `rotate(${this.body.angle}rad)`;
+    }
+}
+
+class Circle {
+    constructor(parentElem, width, color, matterEngine) {
+        this.matterEngine = matterEngine;
+        // initialize DOM element
+        this.element = document.createElement("div");
+        parentElem.appendChild(this.element);
+        // style DOM element
+        this.element.style.width = `${width}px`;
+        this.element.style.height = `${width}px`;
+        this.element.style.backgroundColor = color;
+        this.element.style.position = "absolute";
+        this.element.style.borderRadius = "50%";
+        // initialize matter.js body
+        this.body = Matter.Bodies.circle(0.2 * parentElem.clientWidth, width * -1.8, width / 2);
+        // add matter.js body to matter.js world
+        Matter.Composite.add(this.matterEngine.world, this.body);
+    }
+    // method to update DOM element based on matter.js engine, for rendering
+    updateElem() {
+        // set position and rotation
+        this.element.style.top = `${this.body.position.y - this.element.clientHeight / 2}px`;
+        this.element.style.left = `${this.body.position.x - this.element.clientWidth / 2}px`;
+        this.element.style.transform = `rotate(${this.body.angle}rad)`;
+    }
+}
